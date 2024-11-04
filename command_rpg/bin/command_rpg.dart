5e3a667f2e55bb36eca0b0f8e4d0a913831a7540
chabugon <qnrhs0913@gmail.com>
2024-11-04 17:59:22 +0900

    first commit

---
+++ command_rpg/bin/command_rpg.dart
@@ -0,0 +1,5 @@
+import 'package:command_rpg/command_rpg.dart' as command_rpg;
+
+void main(List<String> arguments) {
+  print('Hello world: ${command_rpg.calculate()}!');
+}
---
+++ command_rpg/test/command_rpg_test.dart
@@ -0,0 +1,8 @@
+import 'package:command_rpg/command_rpg.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
